cb9c42825290f38b9b88d433f741580a2fa83a07
Azavedo <Arjun_Azavedo1@baylor.edu>
2020-01-29 14:50:54 -0600

    Added 4 temps files

--- csv_temps_2.py
+++ csv_temps_2.py
@@ -13,17 +13,24 @@ header_row = next(csv_file)
     #print(index,column_header)
 
 highs = []
+dates = []
 
 for row in csv_file:
     highs.append(int(row[5]))
+    dates.append(row[2])
 
-print(highs[:10])
+#print(highs[:10])
+#print(dates[:10])
 
+fig = plt.figure()
 
-plt.plot(highs, color = 'red')
+plt.plot(dates, highs, color = 'red')
 plt.title("daily high temps, July 2018", fontsize=16)
-plt.xlabel("",fontsize=16)
-plt.ylabel("Temperature (F)",fontsize=16)
-plt.tick_params(axis='both',which="major",labelsize=16)
+plt.xlabel("",fontsize=8)
+plt.ylabel("Temperature (F)",fontsize=12)
+plt.tick_params(axis='both',which="major",labelsize=12)
+
+fig.autofmt_xdate()
+
 
 plt.show()
